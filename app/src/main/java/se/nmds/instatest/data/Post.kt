495e118cbefdb9057a4dba36cc0b94639eded8cf
Mohammed Abunada <m7606225@gmail.com>
2018-10-25 15:00:52 +0200

    Add a text View to show the timestamp but not working yet

--- app/src/main/java/se/nmds/instatest/data/Post.kt
+++ app/src/main/java/se/nmds/instatest/data/Post.kt
@@ -2,4 +2,4 @@ package se.nmds.instatest.data
 
 import java.sql.Timestamp
 
-data class Post(var name: String = "", var url: String = "", var data: Timestamp? = null)
+data class Post(var name: String = "", var url: String = "", var timestamp: Timestamp? = null)
