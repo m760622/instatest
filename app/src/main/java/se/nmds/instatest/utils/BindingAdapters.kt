b1a920f9f49e1d45be3bedba1bd226dac5359b28
Mohammed Abunada <m7606225@gmail.com>
2018-10-25 15:36:22 +0200

    textTimestamp binding Error

--- app/src/main/java/se/nmds/instatest/utils/BindingAdapters.kt
+++ app/src/main/java/se/nmds/instatest/utils/BindingAdapters.kt
@@ -2,9 +2,17 @@ package se.nmds.instatest.utils
 
 import android.databinding.BindingAdapter
 import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
+import java.sql.Timestamp
 
 @BindingAdapter("imageUrl")
 fun setImageUrl(view: ImageView, url: String) {
     Glide.with(view.context).load(url).into(view)
+}
+
+
+@BindingAdapter("textTimestamp")
+fun setTimestamp(view: TextView, value: Timestamp) {
+    view.text = value.toString()
 }
